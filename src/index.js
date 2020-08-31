c4e6ad9bd8cdc71a47326b9c3dec81891159db7f
Aleksey Grygorenko <aleksey2892@gmail.com>
2020-09-01 01:44:58 +0300

    starting project

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'modern-normalize/modern-normalize.css';
+import './styles.css';
+
+ReactDOM.render(document.querySelector('#root'));
