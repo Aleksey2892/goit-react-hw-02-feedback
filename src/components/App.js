108f388c55204ade5bce6e2cff4857428c923f4a
Aleksey Grygorenko <aleksey2892@gmail.com>
2020-09-01 19:57:15 +0300

    ready base features

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,80 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const App = () => {
-  //
-};
+import Statistics from './Statistics';
+
+import s from './styles.module.scss';
+
+class App extends Component {
+  static defaultProps = {
+    startValue: 0,
+  };
+
+  state = {
+    good: this.props.startValue,
+    neutral: this.props.startValue,
+    bad: this.props.startValue,
+    total: this.props.startValue,
+    posFeedback: this.props.startValue,
+  };
+
+  addStat = type => {
+    this.setState(prevState => {
+      return {
+        [type]: prevState[type] + 1,
+      };
+    });
+  };
+
+  countTotalFeedback = () => {
+    this.setState(prevState => {
+      const totalValue = prevState.good + prevState.neutral + prevState.bad;
+      return {
+        total: totalValue,
+      };
+    });
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    this.setState(prevState => {
+      const percentage = Math.round(100 / (prevState.total / prevState.good));
+      return {
+        posFeedback: percentage,
+      };
+    });
+  };
+
+  handleStat = type => {
+    this.addStat(type);
+    this.countTotalFeedback();
+    this.countPositiveFeedbackPercentage();
+  };
+
+  render() {
+    return (
+      <>
+        <div className={s.feedbackBox}>
+          <p className={s.titleFeedback}>Please leave feedback</p>
+
+          <button className={s.button} onClick={() => this.handleStat('good')}>
+            Good
+          </button>
+
+          <button
+            className={s.button}
+            onClick={() => this.handleStat('neutral')}
+          >
+            Neutral
+          </button>
+
+          <button className={s.button} onClick={() => this.handleStat('bad')}>
+            Bad
+          </button>
+        </div>
+
+        <Statistics state={this.state} />
+      </>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'modern-normalize/modern-normalize.css';
-import './styles.css';
 
-ReactDOM.render(document.querySelector('#root'));
+import App from './components/App';
+import 'modern-normalize/modern-normalize.css';
+
+import './base.scss';
+
+ReactDOM.render(<App />, document.querySelector('#root'));
